2d6230f17c61abac9a80388571c1e220d58943b7
Dilawar <dilawar.iitb@gmail.com>
2012-12-10 22:32:05 +0530

    Updated readme file

---
+++ Resources.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML>
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=utf-8">
+	<TITLE>Dilawar Singh</TITLE>
+	<link rel="stylesheet" href="About_Files/dilawar.css">
+	</HEAD><body _textfied="true">
+
+<!-- Site navigation menu -->
+<ul class="navbar">
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/index.php">Home</a></li>
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/About.html">About</a></li>
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Research.html">Research</a></li>
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Writings.html">Writings</a></li>
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Readings.html">Readings</a></li>
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Resources.php">Resources</a></li>
+</ul>
+
+<!-- Main content -->
+<h2>Dilawar Singh</h2>
+<h3>Resources</h3>
+<ul>
+	<li>  Programs written during the course <i>Advanced
+	Computation for Electrical Engineers</i>is available <a
+	href="https://ee717-iitb.googlecode.com/svn/trunk" target="_blank">here</a>.
+<li> My technical blogs and some presentation are also available <a
+	href="http://www.dilawarnotes.wordpress.com" target="_black">here</a>.
+</ul>
+<br>
+
+<h4> EE Department TA's application</h4>
+<a href="http://www.ee.iitb.ac.in/student/%7Edilawar/eeta/eeta.php">EE TA Application (BETA)</a>
+</html>
+
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+	<link rel=StyleSheet href="index_files/dilawar.css" type="text/css" media=screen>
+	<TITLE>Dilawar Singh</TITLE>
+	<!-- Site navigation menu -->
+	</HEAD>
+	<ul class="navbar">
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/index.php">Home</a></li>
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/About.html">About</a></li>
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Research.html">Research</a></li>
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Writings.html">Writings</a></li>
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Readings.html">Readings</a></li>
+  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Resources.php">Resources</a></li>
+</ul>
+</HEAD>
+<BODY>
+<H2><!-- Main content -->Dilawar Singh</H2>
+</SPAN>
+I work in <b>High Performance Computation lab</b>, less commonly known as <br>
+            <b>L.W.C.A </b>(Lab Without a Cool Acronym).<br>     
+    
+<P>Graduate Student 
+<BR>Department of Electrical Engineering <BR>IIT
+Bombay, Powai <BR>Mumbai 400076, India <BR>Email : dilawar [AT]
+ee[DOT]iitb[DOT]ac[DOT]in <br>
+<b> Local contact : </b>Hostel 12D/207
+</P>
+<IMG SRC="images/dilawar_bw.jpg" NAME="Dilawar" WIDTH=260 HEIGHT=260 BORDER=0
+align="left">
+<br>
+<br>
+</BODY>
+</HTML>
