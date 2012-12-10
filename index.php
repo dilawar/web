58ada1a9a980d3d03f918e640ac5c7aa3f402905
Dilawar <dilawar.iitb@gmail.com>
2012-12-11 01:22:32 +0530

    Updated

--- index.php
+++ index.php
@@ -8,23 +8,24 @@ Electrical Engineering</a>. I took my undergraduate degree in
 <tt>Instrumentation and control engineering</tt> from <tt>Some college</tt> in 
 Chennai. I did my <tt>Master in Technology</tt> in <tt>Microelectronics and 
 VLSI</tt> from this institute during 2007-09. <a 
-href="http://www.ee.iitb.ac.in/wiki/faculty/dinesh">DuKS</a> (therefore, I am a 
-<b>duckling</b>) was my thesis advisor on <tt>Fabrication of microelectode 
-arrays for retinal prosthesis</tt>.  I joined back the department in August 2011 
-for pursuing a doctorate.</p>
+href="http://www.ee.iitb.ac.in/wiki/faculty/dinesh">DuKS</a> was my thesis 
+advisor (therefore a duckling) on <tt>Fabrication of microelectode arrays for 
+retinal prosthesis</tt>.  I joined back the department in August 2011 for 
+pursuing a doctorate.</p>
 
 <p>These days, I work in <a href="www.ee.iitb.ac.in/~hpc">High Performance 
 Computation lab</a></b>, less commonly known as <b>L.W.C.A </b>(Lab Without a 
 Cool Acronym).  </p>
 
-<p> If you are looking for a internship in this lab then you are most welcome to 
-send you resume to either to me or to <a 
-href="http://www.ee.iitb.ac.in/~hpc/people.html">other lab members</a>. You 
-should also browse <a href="http://www.ee.iitb.ac.in/wiki/faculty/patkar"> Prof. 
-Patkar's web page </a> </p>
+<h3> Internship and project </h3>
+
+<p> If you are looking for an internship or some project in this lab then you 
+are most welcome to contact <a href="mailto:dilawar@ee.iitb.ac.in">me</a> or to <a 
+href="http://www.ee.iitb.ac.in/~hpc/people.html">other lab members</a> for details. You 
+should also browse <a href="http://www.ee.iitb.ac.in/wiki/faculty/patkar"> Prof.  
+Patkar's web page </a>to know more about lab activities.</p>
     
-<p> This is how is look under normal circumtances. </p>
+<p>This is how I look under normal circumtances. </p>
 
 <IMG SRC="images/dilawar_bw.jpg" NAME="Dilawar" WIDTH=260 HEIGHT=260 BORDER=0
 align="left">
-<br>
