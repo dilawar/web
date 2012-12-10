b2c4f9f7166ca70fa47f37b2ff081a09535a4c74
Dilawar Singh <dilawar@ee.iitb.ac.in>
2012-12-11 01:39:08 +0530

    Launch on sharada.

--- Resources.php
+++ Resources.php
@@ -1,34 +1,19 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<HTML>
-<HEAD>
-	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=utf-8">
-	<TITLE>Dilawar Singh</TITLE>
-	<link rel="stylesheet" href="About_Files/dilawar.css">
-	</HEAD><body _textfied="true">
-
-<!-- Site navigation menu -->
-<ul class="navbar">
-  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/index.php">Home</a></li>
-  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/About.html">About</a></li>
-  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Research.html">Research</a></li>
-  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Writings.html">Writings</a></li>
-  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Readings.html">Readings</a></li>
-  <li><a href="http://www.ee.iitb.ac.in/student/%7Edilawar/Resources.php">Resources</a></li>
-</ul>
-
+<?php 
+include('header.php');
+?>
 <!-- Main content -->
 <h2>Dilawar Singh</h2>
 <h3>Resources</h3>
-<ul>
+<ul class='blog'>
 	<li>  Programs written during the course <i>Advanced
 	Computation for Electrical Engineers</i>is available <a
 	href="https://ee717-iitb.googlecode.com/svn/trunk" target="_blank">here</a>.
-<li> My technical blogs and some presentation are also available <a
+<li> My technical blogs and some presentation are available <a
 	href="http://www.dilawarnotes.wordpress.com" target="_black">here</a>.
 </ul>
-<br>
 
-<h4> EE Department TA's application</h4>
-<a href="http://www.ee.iitb.ac.in/student/%7Edilawar/eeta/eeta.php">EE TA Application (BETA)</a>
-</html>
-
+<h3>Repositories</h3>
+<ul class='blog'>
+<li> My public github <a href='https://github.com/dilawar'>repositories</a>. </li>
+<li> Our lab <a href='http://10.107.105.21'>internal repositories </a>. </li>
+</ul>
